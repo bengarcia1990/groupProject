17a0089bd373385c14c208023ae6e29a436cddb2
bengarcia1990 <garcbr18@gmail.com>
2019-04-22 16:49:56 -0400

    add delete complete

--- bookshelf/src/components/bookshelf.js
+++ bookshelf/src/components/bookshelf.js
@@ -19,24 +19,25 @@ class Bookshelf extends Component {
             books: this.state.books
         });
     }
-    // deleteBook(id) {
-    //     let newBookArray = this.state.books;
-    //     newBookArray.map((book, index) => {
-    //         if (id === book.id) {
-    //             newBookArray.splice(index, 1);
-    //         }
-    //     });
-    //     this.setState({
-    //         book: newBookArray
-    //     })
-    // }
+    deleteBook(id) {
+        let newBookArray = this.state.books;
+        newBookArray.map((book, index) => {
+            if (id === book.id) {
+                newBookArray.splice(index, 1);
+            }
+        });
+        this.setState({
+            book: newBookArray
+        })
+    }
 
     render() {
         return (
             <div className='div-bookshelf'>
                 <div className='row'>
                     {this.state.books.map(book => {
-                        return <Book key={book.id} title={book.id} author={book.author} year={book.year} genre={book.genre} />
+                        return <Book key={book.id} id={book.id} deleteHandler={this.deleteBook.bind(this)} />
+
                     })}
                 </div>
 
